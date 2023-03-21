341202e6b7fa86ebdfc39e666fcdc32ab952d88c
Shahib Hassan <67552203+ShahibHassan@users.noreply.github.com>
2023-03-21 15:56:29 +0000

    code basic operators

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
+let firstOperand = '';
+let operator = '';
+secondOperand = '';
+    
+    function add(a,b) {
+        return a + b;
+    }
 
-const clr = document.getElementById('clear');
+    function subtract(a,b) {
+        return a - b;
+    }
 
+    function multiply(a,b) {
+        return a * b;
+    }
 
+    function divide(a,b) {
+        return a/b;
+    }
 
-const calculator = () => {
-// clear button
-    clr.addEventListener('click', function() {
-    const rslt = document.querySelector('.result');
-    rslt.innerHTML = "";
-    console.log ('cleared');
-});
-
-
+function operate(firstNum, operator, secondNum){
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+            break;
+        case '-':
+            return subtract(firstNum, secondNum);
+            break;
+        case '*':
+            return multiply(firstNum, secondNum);
+            break;
+        case '/':
+            return divide(firstNum, secondNum);
+            break;
+    }
 }
 
-calculator();
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equal');
+const result = document.querySelector('.result');
+
+
+function firstArgs(input) {
+    firstOperand += input;
+    result.textContent = firstOperand;
+}
+
+numbers.forEach(number => {
+    number.addEventListener('click', function() {
+        let value = number.textContent;
+        console.log(value)
+        result.textContent += valuel
+    })
+})
